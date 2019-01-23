2af560b0cd1d2c0b5bdf38711f347b0a7acf95af
Erik Johnson <palehose@gmail.com>
2019-01-23 14:21:52 -0600

    Fix edge case when minion ID is a 16-character string
    Some code in salt._compat which checks if the value is a packed binary
    representation of an IPv6 address fails if the value passed is not a
    bytestring (i.e. a `unicode` type on PY2 or `str` type on PY3). This
    fixes that code when the minion ID is a 16-character string (not a
    bytestring). Note that the minion ID will never be a bytestring as of
    2018.3.0, so this affects any 16-character minion ID when the minion ID
    is checked to see if it is really an IP address.
    
    Closes: #928337
    Origin: upstream, https://github.com/saltstack/salt/pull/51931

--- salt/_compat.py
+++ salt/_compat.py
@@ -189,7 +189,7 @@ class IPv6AddressScoped(ipaddress.IPv6Address):
         :return:
         '''
         packed = False
-        if len(data) == 16 and ':' not in data:
+        if isinstance(data, bytes) and len(data) == 16 and b':' not in data:
             try:
                 packed = bool(int(str(bytearray(data)).encode('hex'), 16))
             except ValueError:
