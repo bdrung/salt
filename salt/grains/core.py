22983805c8ccabcc2333277287b8594ea010439f
Benjamin Drung <benjamin.drung@ionos.com>
2022-02-09 18:43:27 +0100

    grains: Set os grain in each branch in os_data()
    In `os_data()` only the kernel branch for `SunOS` might not set the `os`
    grain. To make the code easier to read, ensure that `os` is set in each
    branch in `os_data()`.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@ionos.com>

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -2335,6 +2335,8 @@ def os_data():
             grains.update(_smartos_os_data())
         elif os.path.isfile("/etc/release"):
             grains.update(_sunos_release())
+        else:
+            grains["os"] = "Unknown {}".format(grains["kernel"])
         grains.update(_sunos_cpudata())
     elif grains["kernel"] == "VMkernel":
         grains["os"] = "ESXi"
@@ -2382,9 +2384,6 @@ def os_data():
     else:
         grains["os"] = grains["kernel"]
         grains["os_family"] = "Unknown"
-
-    if not grains["os"]:
-        grains["os"] = "Unknown {}".format(grains["kernel"])
 
     grains["osarch"] = _osarch(grains.get("os_family"), grains.get("cpuarch"))
 
